b356bcb0cc02c9d1c30eb5fafb2f087a00e499d1
Rio Bagus Rinanto <riobr@ACSET.CO>
2022-09-14 09:55:44 +0700

    tes edit server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bcrypt = require('bcryptjs');
 const configsettings = require('./server/config');
 const timeout = require('connect-timeout'); //express v4
 
-//Cors
+//Corsz
 app.use(cors());
 app.options('*', cors());
 //registering view
